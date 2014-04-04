d1c5e654d0a85b422d831c63d2d38986ed3d24fb
Andy Friesen <andy.friesen@gmail.com>
2014-04-03 22:28:20 -0700

    Drop all the Rust code.

--- Makefile
+++
@@ -1,9 +0,0 @@
-
-bin/main: main.rs lex.rs
-	rustc main.rs -o bin/main
-
-bin/tests: lex.rs lextest.rs
-	rustc --test lextest.rs -o bin/tests
-
-runtests: bin/tests
-	./bin/tests
--- parsetest.rs
+++
@@ -1,8 +0,0 @@
-mod parse;
-
-#[test]
-fn join() {
-    let p = parse::JoinParser { p1: ~parse::ParseIdentifier
-                              , p2: ~parse::ParseKeyword
-                              };
-}
